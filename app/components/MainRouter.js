a81cc7fb4bbd6ef266ec42d98de093f3c3d21e53
Aizhan87 <moonsoul87@hotmail.com>
2019-08-11 11:58:24 -0700

    added links to navbar so the page will not reload

--- app/components/MainRouter.js
+++ app/components/MainRouter.js
@@ -9,6 +9,7 @@ import Login from "./Login";
 import Signup from "./Signup";
 import AboutUs from "./AboutUs";
 import Donate from "./Donate";
+import MeetFamilies from "./MeetFamilies";
 import axios from 'axios';
 
 export default class MainRouter extends Component {
@@ -49,17 +50,17 @@ export default class MainRouter extends Component {
   render() {
     return (
       <Router>
-      	<Switch>
-          <Route exact path="/" render={props => 
+        <Switch>
+          <Route exact path="/" render={props =>
             <Main
               {...props}
               authenticate={this.authenticate}
               deAuthenticate={this.deAuthenticate}
               authenticated={this.state.authenticated}
               logout={this.logout}
-            />} 
+            />}
           />
-          <Route exact path="/login" render={props => 
+          <Route exact path="/login" render={props =>
             <Login
               {...props}
               authenticate={this.authenticate}
@@ -68,46 +69,46 @@ export default class MainRouter extends Component {
               logout={this.logout}
             />}
           />
-          <Route exact path="/signup" render={props => 
+          <Route exact path="/signup" render={props =>
             <Signup
               {...props}
               authenticate={this.authenticate}
               deAuthenticate={this.deAuthenticate}
               authenticated={this.state.authenticated}
               logout={this.logout}
-            />} 
+            />}
           />
-           {/* Thatiana added this code for the donate page */}
-          <Route exact path="/donate" render={props => 
+          {/* Thatiana added this code for the donate page */}
+          <Route exact path="/donate" render={props =>
             <Donate
               {...props}
               authenticate={this.authenticate}
               deAuthenticate={this.deAuthenticate}
               authenticated={this.state.authenticated}
               logout={this.logout}
-            />} 
+            />}
           />
           {/* Thatiana added this code for the meet families page */}
-          <Route exact path="/meetfamilies" render={props => 
-            <Donate
+          <Route exact path="/meetfamilies" render={props =>
+            <MeetFamilies
               {...props}
               authenticate={this.authenticate}
               deAuthenticate={this.deAuthenticate}
               authenticated={this.state.authenticated}
               logout={this.logout}
-            />} 
+            />}
           />
           {/* Thatiana added this code for the about page */}
-          <Route path="/aboutus" render={props => 
+          <Route path="/aboutus" render={props =>
             <AboutUs
               {...props}
               authenticate={this.authenticate}
               deAuthenticate={this.deAuthenticate}
               authenticated={this.state.authenticated}
               logout={this.logout}
-            />} 
+            />}
           />
-      	</Switch>
+        </Switch>
       </Router>
     );
   }
