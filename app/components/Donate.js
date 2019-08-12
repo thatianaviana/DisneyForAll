6c8ff33e948000789ff8a71cd6e10aed3edbe03e
Aizhan87 <moonsoul87@hotmail.com>
2019-08-12 16:47:53 -0700

    styled donation page

--- app/components/Donate.js
+++ app/components/Donate.js
@@ -13,31 +13,17 @@ export default class Donate extends Component {
                     deAuthenticate={this.props.deAuthenticate}
                     logout={this.props.logout}
                 />
-                <section id="plans">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-12 text-center">
-                                <div className="panel panel-danger panel-pricing">
-                                    <div className="panel-heading">
-                                        <h3>Donate</h3>
-                                    </div>
-                                    <div className="panel-body text-center">
-                                        <p><strong>$100 / Month</strong></p>
-                                        <div id="paypal-button-container"></div>
-                                    </div>
-                                    <ul className="list-group text-center">
-                                        <li className="list-group-item"><i className="fa fa-check"></i> Personal use</li>
-                                        <li className="list-group-item"><i className="fa fa-check"></i> Unlimited projects</li>
-                                        <li className="list-group-item"><i className="fa fa-check"></i> 27/7 support</li>
-                                    </ul>
-                                    <div className="panel-footer">
-                                        <a className="btn btn-lg btn-block btn-danger" href="#">BUY NOW!</a>
-                                    </div>
-                                </div>
+                <div className="donation">
+                    <div className="row">
+                        <div className="col-md-12 text-center">
+                            <h1>Disney For All</h1>
+                            <h3>Because Every Child Deserves A Day In The Happiest Place On Earth</h3>
+                            <div id="paypal-button-container">
+                                <h3>Make a donation</h3>
                             </div>
                         </div>
                     </div>
-                </section>
+                </div>
             </div>
         );
     }
