a46cc35a75697a5b7e52fe693ebbe3fe31d575ec
Thatiana <cruz.thatiana@gmail.com>
2019-08-17 10:38:02 -0700

    blah

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -16,12 +16,12 @@ export default class Footer extends Component {
     render() {
         return (
             <div>
-                <footer>
+                <footer className="sticky-bottom">
                     <div className="container text-center">
                         <p>Copyright &copy; Disney For All is a registered 501(c)3 non-profit organization. 100% of your donation is tax-deductible.</p>
-                        <i className="fab fa-twitter fa-2x"></i>
-                        <i className="fab fa-facebook fa-2x"></i>
-                        <i className="fab fa-instagram fa-2x"></i>
+                        <a href="https://twitter.com/?lang=en"> <i className="fab fa-twitter"></i></a>
+                        <a href="https://www.facebook.com"><i className="fab fa-facebook"></i></a>
+                        <a href="https://www.instagram.com"><i className="fab fa-instagram"></i></a>
                     </div>
                 </footer>
             </div>
