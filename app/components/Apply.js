03dfae24620d9bf1dffd6295306679b58569e2a9
Paula Villegas <vpaula90@gmail.com>
2019-08-17 13:16:38 -0700

    uploading my changes for apply

--- app/components/Apply.js
+++ app/components/Apply.js
@@ -1,119 +1,277 @@
+// Include React
 import React, { Component } from 'react';
-import { Link, Redirect } from 'react-router-dom';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+import Auth from './utils/Auth';
 import Nav from './children/Nav'
 
 require('./Apply.css');
 
 export default class Apply extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          fullName: '',
-          income: '',
-          aboutFamily: '',
-          message:''
-        };
-    
-        this.handleOnClick = this.handleOnClick.bind(this);
-      }
-// state = {
-//     message: ""
-// }
+	constructor(props) {
+		super(props);
+		this.state = {
+			value: '',
+			fullName: '',
+			houseHoldIncome: '',
+			// houseHoldIncomeRepeat: '',
+			aboutFamily: '',
+			// aboutFamilyRepeat: ''
+		};
 
-handleOnClick() {
+		this.handlefullNameValidation = this.handlefullNameValidation.bind(this);
+		this.handlehouseHoldIncomeValidation = this.handlehouseHoldIncomeValidation.bind(this);
+		// this.handlehouseHoldIncomeRepeat = this.handlehouseHoldIncomeRepeat.bind(this);
+		this.handleaboutFamilyValidation = this.handleaboutFamilyValidation.bind(this);
+		// this.handleaboutFamilyRepeat = this.handleaboutFamilyRepeat.bind(this);
+		// this.signUpUser = this.signUpUser.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
-    let fullName = $("#fullName").val();
-    let familyIncome = $("#income").val();
-    let aboutFamily = $("#aboutFamily").val();
-    console.log("onclick", fullName, familyIncome, aboutFamily)
+	handlefullNameValidation(event) {
+		// fullName is passed in
+		const fullNameVal = this.refs.fullName.value,
+			fullNameForm = this.refs.fullNameForm,
+			fullNameFeedback = this.refs.fullNameFeedback;
+		// fullName is updated in state
+		this.setState({
+			'fullName': fullNameVal
+		});
 
-    let object = {
-        fullName: fullName,
-        income: familyIncome,
-        aboutFamily: aboutFamily
-    }
+		// fullName is checked to see if it matches certain length. If not, the screen will indicate it as such.
+		if (fullNameVal.length < 6) {
+			fullNameForm.classList.remove("has-success");
+			fullNameForm.classList.add("has-error");
+			fullNameFeedback.textContent = "fullName must be at least 6 characters long";
+		} else {
+			fullNameForm.classList.remove("has-error");
 
-    axios.post('/apply/new', object)
-    .then(function (data) {
-      console.log(data);
-    //   if (data.data.success) {
-    //     this.props.authenticate();
-    //     this.setState({
-    //       redirectToReferrer: true
-    //     });
-    //   } else {
-    //     alert(data.data.message);
-      })
-    }
-//     // axios call
-//     //("/apply/new", object, function (data) {
-//     //    console.log(data)
-//     // })
-//     // need to build object to send Ajax call to database
-//     // ajax call for update database in apply routes file = /api/apply/new
-//     // what to do when come back
+			fullNameForm.classList.add("has-success");
+			fullNameFeedback.textContent = "fullName valid!";
+		}
+	}
 
+	handlehouseHoldIncomeValidation(event) {
 
-render() {
-    return (
-        <div>
-            <Nav
-                authenticated={this.props.authenticated}
-                authenticate={this.props.authenticate}
-                deAuthenticate={this.props.deAuthenticate}
-                logout={this.props.logout}
-            />
+		// houseHoldIncome is passed in
+		const houseHoldIncomeVal = this.refs.houseHoldIncome.value;
+		const houseHoldIncomeForm = this.refs.houseHoldIncomeForm;
+		const houseHoldIncomeFeedback = this.refs.houseHoldIncomeFeedback;
 
-            <section id="plans">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12 text-center">
-                            <div className="panel panel-danger panel-pricing">
-                                <div className="panel-heading">
-                                    <h3>To Apply Please Fill Out All The Information Below</h3>
-                                </div>
+		this.setState({
+			'houseHoldIncome': houseHoldIncomeVal
+		});
 
-                                <form>
-                                    <div className="form-group col-md-6 text-left">
-                                        <p>Full Name</p>
-                                        <input type="name" className="form-control" id="fullName" />
-                                    </div>
-                                    <div className="form-group col-md-6 text-left">
-                                        <p>Annual Household Income</p>
-                                        <input type="amount" className="form-control" id="income" />
-                                    </div>
+		const houseHoldIncomeRegEx = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/;
+		if (!houseHoldIncomeRegEx.test(houseHoldIncomeVal)) {
+			houseHoldIncomeForm.classList.remove("has-success");
+			houseHoldIncomeForm.classList.add("has-error");
+			houseHoldIncomeFeedback.textContent = "houseHoldIncome must contain at least 1 lowercase letter, 1 uppercase letter, 1 number, 1 special character and must be at least 8 characters long.";
+		} else {
+			houseHoldIncomeForm.classList.remove("has-error");
 
-                                    {/* <div class="input-group mb-3 text-left"/>
-                                    <div class="input-group-prepend">
-                                        <p>Annual Income</p>
-                                        <span class="input-group-text">$</span>
-                                </div>
- */}
-                                    <div className="form-group col-md-10 text-left">
-                                        <p>About My Family</p>
-                                        <textarea className="form-control" id="aboutFamily" rows="3"></textarea>
-                                    </div>
+			houseHoldIncomeForm.classList.add("has-success");
+			houseHoldIncomeFeedback.textContent = "houseHoldIncome set correctly!";
+		}
+	}
 
-                                    <div className="add-file col-md-6 text-left">
-                                        <p>Share a picture of your family</p>
-                                        <input type="file" id="familyPicture" />
-                                    </div>
+	handlehouseHoldIncomeRepeat(event) {
+		const houseHoldIncomeVal = this.state.houseHoldIncome;
+		const houseHoldIncomeRepeat = this.refs.repeathouseHoldIncome.value;
+		const repeathouseHoldIncomeForm = this.refs.repeathouseHoldIncomeForm;
+		const repeathouseHoldIncomeFeedback = this.refs.repeathouseHoldIncomeFeedback;
 
-                                </form>
+		this.setState({
+			'houseHoldIncomeRepeat': houseHoldIncomeRepeat
+		});
 
-                                <br></br>
-                                <div className="panel-footer">
-                                    <a onclick={this.handleOnClick()} className="btn btn-lg btn-block btn-success" id="submit" href="#">Submit!</a>
-                                </div>
-                                {/* onclick={this.handleOnClick()} */}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
-    );
-}
+		if (houseHoldIncomeVal !== houseHoldIncomeRepeat) {
+			repeathouseHoldIncomeForm.classList.remove("has-success");
+
+			repeathouseHoldIncomeForm.classList.add("has-error");
+			repeathouseHoldIncomeFeedback.textContent = "houseHoldIncomes Don't Match";
+		} else {
+			repeathouseHoldIncomeForm.classList.remove("has-error");
+
+			repeathouseHoldIncomeForm.classList.add("has-success");
+			repeathouseHoldIncomeFeedback.textContent = "houseHoldIncomes Match!";
+		}
+	}
+
+	handleaboutFamilyValidation(event) {
+
+		const aboutFamilyVal = this.refs.aboutFamily.value;
+		const aboutFamilyForm = this.refs.aboutFamilyForm;
+		const aboutFamilyFeedback = this.refs.aboutFamilyFeedback;
+		const aboutFamilyAdditionalFeedback = this.refs.aboutFamilyAdditionalFeedback;
+		const aboutFamilyRegEx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+		this.setState({
+			'aboutFamily': aboutFamilyVal
+		});
+
+		if (!aboutFamilyRegEx.test(aboutFamilyVal)) {
+			aboutFamilyForm.classList.remove("has-success");
+
+			aboutFamilyForm.classList.add("has-error");
+			aboutFamilyFeedback.textContent = "Invalid aboutFamily";
+			aboutFamilyAdditionalFeedback.textContent = "Ex: someone@example.com";
+
+		} else {
+			aboutFamilyForm.classList.remove("has-error");
+
+			aboutFamilyForm.classList.add("has-success");
+			aboutFamilyFeedback.textContent = "Valid aboutFamily!";
+			aboutFamilyAdditionalFeedback.textContent = "";
+		}
+	}
+
+	handleaboutFamilyRepeat(event) {
+
+		const aboutFamilyVal = this.refs.aboutFamilyRepeat.value;
+		const aboutFamilyForm = this.refs.aboutFamilyRepeatForm;
+		const aboutFamilyFeedback = this.refs.aboutFamilyRepeatFeedback;
+		const aboutFamilyRegEx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+		this.setState({
+			'aboutFamilyRepeat': aboutFamilyVal
+		});
+
+		if (!aboutFamilyRegEx.test(aboutFamilyVal)) {
+			aboutFamilyForm.classList.remove("has-success");
+
+			aboutFamilyForm.classList.add("has-error");
+			aboutFamilyFeedback.textContent = "aboutFamilys Don't Match";
+
+		} else {
+			aboutFamilyForm.classList.remove("has-error");
+
+			aboutFamilyForm.classList.add("has-success");
+			aboutFamilyFeedback.textContent = "aboutFamilys Match!";
+		}
+	}
+
+	signUpUser(userData) {
+		axios.post("/apis/users/signup", {
+			fullName: userData.fullName,
+			aboutFamily: userData.email,
+			houseHoldIncome: userData.houseHoldIncome
+		}).then(function (data) {
+			console.log("data stuff", data.data);
+			if (data.duplicateUser) {
+				// Replace with Modal
+				alert("Sorry, that fullName has been taken");
+			} else if (data.data.success) {
+				console.log("yay!")
+				this.props.authenticate();
+				this.setState({
+					redirectToReferrer: true
+				});
+			}
+		}.bind(this)).catch(function (err) {
+			console.log(err);
+		});
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+
+		const fullName = this.state.fullName;
+		const email = this.state.email;
+		const houseHoldIncome = this.state.houseHoldIncome;
+
+		let userData = {
+			fullName: fullName,
+			email: email,
+			houseHoldIncome: houseHoldIncome
+		};
+
+		if (!userData.fullName || !userData.email || !userData.houseHoldIncome) {
+			return alert("Please don't leave fields blank");
+		}
+
+		// If we have an email and houseHoldIncome, run the signUpUser function
+		this.signUpUser(userData);
+
+		this.setState({
+			value: '',
+			fullName: '',
+			houseHoldIncome: '',
+			houseHoldIncomeRepeat: '',
+			email: '',
+			emailRepeat: '',
+			redirectToReferrer: false
+		});
+	}
+
+	render() {
+		const { from } = this.props.location.state || { from: { pathname: '/' } };
+		const { redirectToReferrer } = this.state;
+
+		if (redirectToReferrer) {
+			return (
+				<Redirect to={from} />
+			)
+		}
+
+		return (
+			<div>
+				<Nav
+					authenticated={this.props.authenticated}
+					authenticate={this.props.authenticate}
+					deAuthenticate={this.props.deAuthenticate}
+					logout={this.props.logout}
+				/>
+				<div id="registration-container" className="container-fluid">
+					<section className="container">
+						<div className="container-page">
+							<form onSubmit={this.handleSubmit.bind(this)}>
+								<div className="col-md-6">
+									<h3 className="dark-grey">Registration</h3>
+
+									{/* <div id="fullName-form" ref="fullNameForm" className="form-group col-lg-12">
+										<label>fullName</label>
+										<input type="" name="" ref="fullName" className="form-control" id="fullName-input" value={this.state.fullName} onChange={this.handlefullNameValidation} />
+										<small id="fullName-feedback" ref="fullNameFeedback" className=""></small>
+									</div> */}
+									<div id="email-form" className="form-group col-lg-12" ref="emailForm">
+										<label>Email Address</label>
+										<input type="email" name="" ref="email" className="form-control" id="email-input" value={this.state.email} onChange={this.handleEmailValidation} />
+
+										<p id="email-feedback" className="" ref="emailFeedback"></p>
+										<small id="email-additional-feedback" ref="emailAdditionalFeedback" className="form-text text-muted"></small>
+									</div>
+
+									<div id="email-repeat-form" className="form-group col-lg-12" ref="emailRepeatForm">
+										<label>Repeat Email Address</label>
+										<input type="email" name="" ref="emailRepeat" className="form-control" id="repeat-email-input" value={this.state.emailRepeat} onChange={this.handleEmailRepeat} />
+										<small id="email-repeat-feedback" className="" ref="emailRepeatFeedback"></small>
+									</div>
+									
+									<div id="houseHoldIncome-form" className="form-group col-lg-12" ref="houseHoldIncomeForm">
+										<label>houseHoldIncome</label>
+										<input type="houseHoldIncome" name="" ref="houseHoldIncome" className="form-control" id="houseHoldIncome-input" value={this.state.houseHoldIncome} onChange={this.handlehouseHoldIncomeValidation} />
+										<small id="houseHoldIncome-feedback" ref="houseHoldIncomeFeedback" className=""></small>
+									</div>
+
+									<div id="repeat-houseHoldIncome-form" className="form-group col-lg-12" ref="repeathouseHoldIncomeForm">
+										<label>Repeat houseHoldIncome</label>
+										<input type="houseHoldIncome" name="" ref="repeathouseHoldIncome" className="form-control" id="repeat-houseHoldIncome-input" value={this.state.houseHoldIncomeRepeat} onChange={this.handlehouseHoldIncomeRepeat} />
+										<small id="repeat-houseHoldIncome-feedback" className="" ref="repeathouseHoldIncomeFeedback"></small>
+									</div>
+
+									<div className="col-md-6">
+										<button type="submit" className="btn btn-primary signup">Register</button>
+									</div>
+
+								</div>
+								
+							</form>
+						</div>
+					</section>
+				</div>
+			</div>
+		)
+	}
 }
