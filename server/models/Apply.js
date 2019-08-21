99bd096a4a046ef25953d398d1a2e2c46432d7ef
Paula Villegas <vpaula90@gmail.com>
2019-08-20 21:23:02 -0700

    adding updates to apply

--- server/models/Apply.js
+++ server/models/Apply.js
@@ -5,24 +5,40 @@
 var mongoose = require('mongoose');
 
 // define the schema for our user model
-var applySchema = mongoose.Schema({
+// var Schema = mongoose.Schema;
+
+const applySchema = mongoose.Schema({
 
     fullName: {
         type: String,
-        min: [1, 'Too few characters'],
-        max: 100,
-        required: [true, 'Please enter your Full Name.']
+        // min: [1, 'Too few characters'],
+        // max: 100,
+        // required: [true, 'Please enter your Full Name.']
     },
     houseHoldIncome: {
-        type: Number,
-        required: [true, 'Please enter your yearly Household Income']
+        type: String,
+        // required: [true, 'Please enter your yearly Household Income']
     },
     aboutFamily: {
         type: String,
-        required: [true, 'Please tell us about your family']
+        // required: [true, 'Please tell us about your family']
     },
 
 });
 
 // create the model for users and expose it to our app
 module.exports = mongoose.model('Apply', applySchema);
+
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+// const ItemSchema = new Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     date: {
+//         type: Date,
+//         default: Date.now
+//     }
+// });
+// module.exports = Item = mongoose.model('item', ItemSchema)
